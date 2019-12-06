f92d6da98ccbe614d48be0349572072d175d4dcc
Hemaal Hansda <hemaal.hansda@seenit.in>
2019-12-06 18:00:49 +0530

    added ui of the form

--- app/components/CreateProject.js
+++ app/components/CreateProject.js
@@ -9,7 +9,12 @@ import {
     TouchableHighlight,
     Alert,
     TextInput,
+    Button,
 } from 'react-native';
+
+import Textarea from 'react-native-textarea';
+
+import { Card } from 'react-native-shadow-cards';
 
 export default class CreateProject extends React.Component {
     constructor(props) {
@@ -20,10 +25,25 @@ export default class CreateProject extends React.Component {
     render() {
         return(
             <View style={styles.formContainer}>
-                <Text>Enter Title</Text>
-                <TextInput style={styles.titleInput}></TextInput>
-                <Text>Enter Description</Text>
-                <TextInput style={styles.titleInput}></TextInput>
+                <Card style={styles.subForm}>
+                    <Text>T I T L E</Text>
+                    <TextInput style={styles.titleInput} placeholder="Enter the title"></TextInput>
+                    <Text>D E S C R I P T I O N</Text>
+                    <Textarea
+                        containerStyle={styles.textareaContainer}
+                        style={styles.textarea}
+                        onChangeText={this.onChange}
+                        defaultValue={this.state.text}
+                        maxLength={120}
+                        placeholderTextColor={'#c7c7c7'}
+                        underlineColorAndroid={'transparent'}
+                        placeholder="Enter the description"></Textarea>
+                </Card>
+                <Button
+                    style={styles.submitButton}
+                    title="Submit Project"
+                    onPress={() => Alert.alert('Simple Button pressed')}
+                    />
             </View>
         );
     }
@@ -33,9 +53,61 @@ const styles = StyleSheet.create({
     formContainer: {
         padding: 20,
         margin: 20,
+        height: '95%',
+        flex: 1,
+        display: 'flex',
+    },
+    subForm: {
+        marginBottom: 100,
+        padding: 10,
+        width: '100%',
     },
     titleInput: {
-        backgroundColor: 'grey',
-        borderBottomColor: 'black'
+        backgroundColor: '#e8e8e8',
+        borderBottomColor: 'black',
+        borderRadius: 5,
+        marginTop: 5,
+        marginBottom: 5,
+        padding: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 14,
+        height: 50,
+        shadowOffset: {  width: 10,  height: 10,  },
+    },
+    desInput: {
+        backgroundColor: '#e8e8e8',
+        borderBottomColor: 'black',
+        borderRadius: 5,
+        marginTop: 5,
+        marginBottom: 5,
+        padding: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 14,
+        height: 80,
+        shadowOffset: {  width: 10,  height: 10,  },
+        justifyContent: 'flex-start',
+    },
+    submitButton: {
+        padding: 5,
+        margin: 40,
+        borderRadius: 5,
+        height: 100,
+        position: 'absolute',
+        bottom: 0,
+    },
+    textareaContainer: {
+        height: 100,
+        padding: 5,
+        borderRadius: 5,
+        shadowOffset: {  width: 10,  height: 10,  },
+        backgroundColor: '#e8e8e8',
+    },
+    textarea: {
+        textAlignVertical: 'top',  // hack android
+        height: 100,
+        fontSize: 14,
+        color: '#333',
     },
 });
